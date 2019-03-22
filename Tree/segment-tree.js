600df3b95859e11d2fff1a2fa78ee5594d36a922
GoodLuckDude <tsirkmaks@mail.ru>
2019-03-22 21:45:44 +0500

    fix 'fn in segmentTree

--- Tree/segment-tree.js
+++ Tree/segment-tree.js
@@ -12,30 +12,33 @@ function dummySegmentTree(array, fn, N) {
 
 function segmentTree(array, fn, N) {
   var tree = [];
-  var tree2 = [];
   function build (pos, tl, tr) {
     if (tr < 0) {return}
     if (tl == tr) {
-      if ( Array.isArray(array[tl]) ) {
-        tree[pos] = segmentTree(array[tl])
-      }
-      tree[pos] = array[tl]
-    }   //how to organize?
-    else {
+      tree[pos] = fn(array[tl], N) 
+    } else {
       var tm = Math.floor((tl + tr) / 2);
       build(pos*2, tl, tm);
       build(pos*2+1, tm+1, tr);
-      tree[pos] = tree[pos*2] + tree[pos*2+1];
+      tree[pos] = fn(tree[pos*2], tree[pos*2+1]);
     }
   };
   build(1, 0, array.length-1);
-  // return function(from, to){
-  //   if (from < 0 || to < from || to > array.length) {throw new Error("This range is not valid")}
-  //   var range = array.slice(from, to);
-  //   return range.reduce(function(answer, current) {
-  //     return fn(answer, current)
-  //   }, N);
-  //};
+  return function(from, to){
+    if (from < 0 || to < from || to > array.length) {throw new Error("This range is not valid")}
+    function count (pos, tl, tr)
+
+
+
+
+
+    var range = array.slice(from, to);
+    return range.reduce(function(answer, current) {
+      return fn(answer, current)
+    }, N);
+
+
+  };
   // return function(from, to) {
   //   function foo (pos, tl, tr) {
   //     if (from == tl && to == tr) {
@@ -43,9 +46,8 @@ function segmentTree(array, fn, N) {
   //     }
   // }
   // }
-  return tree
 };
-tree = segmentTree([[1, 2, 3], [1, 0, 0], [0, 0, 1]], sum, 0);
+tree = recursiveSegmentTree([1, 2, 3, 4,], mul, 1);
 var i = 10;
 function recursiveSegmentTree(array, fn, N) {
   return segmentTree(array, fn, N);
--- Tree/tree.js
+++ Tree/tree.js
@@ -1,1 +1,2 @@
 function sum(a = 0, b = 0) { return a + b }
+function mul(a, b) { return a * b }
