d425bffebbcabe42d516e2ca304ae0b286067dfb
GoodLuckDude <tsirkmaks@mail.ru>
2019-03-28 10:21:55 +0500

    максимСвин

--- Tree/segment-tree.js
+++ Tree/segment-tree.js
@@ -31,99 +31,101 @@ function create(array, fn, N) {
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // var result;
-  // if (!Array.isArray(array[0])) {
-  //   return dummySegmentTree(array, fn, N)
-  // }
-  
-  // return(function(from, to) {
-  //   return function (l, r) {
-  //     var answer = N;
-  //     for (let i = from; i < to; i++) {
-  //       answer = fn(dummySegmentTree(array[i], fn, N)(l, r), answer)
-  //     }
-  //     return answer
-  //   }
-  // })
+  }
+}
+function fnToRecurce(a, b, fn, N){
+  if (Array.isArray(a)) {
+    var small;
+    var big = function(a, b) {
+      if ( a.length - b.length < 0 ) {
+        small = b;
+        return a
+      }
+      small = a;
+      return b
+    }(a, b)
+    return big.map(function (item, i) {
+      return fnToRecurce(item, small[i], fn, N)
+    });
+  } else {
+    return fn(a, b);
   }
 }
 
-
-var a = [ 
-  [0, 3, 5],
-  [1, 3, 5],
-  [1, 3, 5]
-];
-
-var j = create(a, mul, 1);
-alert (j(0, 3)(0,3));
-
-
-function segmentTree(array, fn, N) {
+function buildTree(array, fn, N) {
   var tree = [];
   function build (pos, tl, tr) {
     if (tr < 0) {return}
-    if (tl == tr)  {
+    if (tl == tr) {
+      if (Array.isArray(array[tl])) {
+        tree[pos] = buildTree(array[tl], fn, N)
+      } else {
       tree[pos] = array[tl];
-      if (Array.isArray(array[tl])){
-        array[tl] = (segmentTree(array[tl], fn, N));
       }
     } else {
       var tm = Math.floor((tl + tr) / 2);
       build(pos*2+1, tl, tm);
       build(pos*2+2, tm+1, tr);
-      tree[pos] = fn(tree[pos*2+1], tree[pos*2+2]);
+      tree[pos] = fnToRecurce(tree[pos*2+1], tree[pos*2+2], fn, N)
     }
   };
   build(0, 0, array.length-1);
+  return tree;
+}
 
-  return function(from, to){
-    if (to == from) {return N}
+function segmentTree(array, fn, N) {
+  var tree = buildTree(array,fn, N);
+  function returnN(segment) {
+    if (Array.isArray(segment)) {
+      return [returnN(segment[0])]
+    } else {
+      return N
+    }
+  }
+  return function fromTo(from, to){
+    if (from == to) {return returnN(tree[0])}
     if (from < 0 || to > array.length || to < from) {throw new Error("This range is not valid")}
     function count (pos, tl, tr){
-      if (to-1 < tl || from > tr || tr < 0) {return N}
+      if (to-1 < tl || from > tr || tr < 0) {return returnN(tree[0])}
       if (tl >= from && tr <= to-1) {return tree[pos]}
       var tm = Math.floor((tl + tr) / 2);
-      return fn(count(pos*2+1, tl, tm), count(pos*2+2, tm+1, tr))
+      return fnToRecurce(count(pos*2+1, tl, tm), count(pos*2+2, tm+1, tr), fn, N)
     };
-    return count(0, 0, array.length-1)
-
-    // var range = array.slice(from, to);
-    // return range.reduce(function(answer, current) {
-    //   return fn(answer, current)
-    // }, N);
+    result = count(0, 0, array.length-1);
+    if (Array.isArray(result)) {
+      tree = result;
+      return fromTo;
+    } else {
+      return result
+    }
 
   };
 };
 
 
 function recursiveSegmentTree(array, fn, N) {
-  return create(array, fn, N);
+  return segmentTree(array, fn, N)  
 }
+
+var ar = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+  [13, 14, 15, 16],
+]
+
+var eee = [ 
+  [136, 60, 76, 28, 32, 36, 40]
+  [36, 14, 22, 6, 8, 10, 12],
+  [100, 46, 54, 22, 24, 26, 28],
+  [10, 3, 7, 1, 2, 3, 4],
+  [26, 11, 15, 5, 6, 7, 8],
+  [42, 19, 23, 9, 10, 11, 12],
+  [58, 27, 31, 13, 14, 15, 16],
+];
+var g = segmentTree(ar, sum, 0);
+g = g(0, 2)(0, 2);
+g = g(0, 2);
 
 function getElfTree(array) {
   return recursiveSegmentTree(array, sum, 0);
