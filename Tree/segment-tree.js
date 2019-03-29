61c7ffe202af950f6cd9c599d1ffc6bb10e859ca
GoodLuckDude <tsirkmaks@mail.ru>
2019-03-29 10:28:28 +0500

    n-segmentTree (need to fix "tree")

--- Tree/segment-tree.js
+++ Tree/segment-tree.js
@@ -37,18 +37,22 @@ function fnToRecurce(a, b, fn, N){
   if (Array.isArray(a)) {
     var small;
     var big = function(a, b) {
-      if ( a.length - b.length < 0 ) {
+      if ( a.length - b.length > 0 ) {
         small = b;
         return a
       }
       small = a;
       return b
     }(a, b)
-    return big.map(function (item, i) {
-      return fnToRecurce(item, small[i], fn, N)
+    var newAr = big.map(function (item, i) {
+      if (!small[i]) {return item}
+      var result = fnToRecurce(item, small[i], fn, N);
+      return result;
     });
+    return newAr
   } else {
-    return fn(a, b);
+    var answer = fn(a, b);
+    return  answer
   }
 }
 
@@ -76,22 +80,26 @@ function buildTree(array, fn, N) {
 function segmentTree(array, fn, N) {
   var tree = buildTree(array,fn, N);
   function returnN(segment) {
-    if (Array.isArray(segment)) {
-      return [returnN(segment[0])]
+    if (Array.isArray(segment[0])) {
+      var newN = [];
+      for (let i = 0; i < segment[0].length; i++){
+        newN.push(returnN(segment[0]))
+      }
+      return newN
     } else {
       return N
     }
   }
   return function fromTo(from, to){
-    if (from == to) {return returnN(tree[0])}
+    if (from == to) {return returnN(tree)}
     if (from < 0 || to > array.length || to < from) {throw new Error("This range is not valid")}
     function count (pos, tl, tr){
-      if (to-1 < tl || from > tr || tr < 0) {return returnN(tree[0])}
+      if (to-1 < tl || from > tr || tr < 0) {return returnN(tree)}
       if (tl >= from && tr <= to-1) {return tree[pos]}
       var tm = Math.floor((tl + tr) / 2);
       return fnToRecurce(count(pos*2+1, tl, tm), count(pos*2+2, tm+1, tr), fn, N)
     };
-    result = count(0, 0, array.length-1);
+    result = count(0, 0, (tree.length-1)/2);
     if (Array.isArray(result)) {
       tree = result;
       return fromTo;
@@ -108,14 +116,24 @@ function recursiveSegmentTree(array, fn, N) {
 }
 
 var ar = [
-  [1, 2, 3, 4],
-  [5, 6, 7, 8],
-  [9, 10, 11, 12],
-  [13, 14, 15, 16],
-]
+  // [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]],
+  // [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]],
+  // [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]],
+  // [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]],
+  [[0, 1], [0, 0], [1, 1], [0, 0]],
+  [[0, 0], [1, 0], [1, 0], [0, 1]],
+  [[0, 0], [1, 0], [0, 1], [1, 0]],
+  [[1, 0], [0, 0], [0, 0], [0, 0]],
+  // [1, 0, 1, 1],
+  // [0, 1, 0, 0],
+  // [0, 0, 0, 1],
+  // [1, 1, 1, 1]
+];
+
+
 
 var eee = [ 
-  [136, 60, 76, 28, 32, 36, 40]
+  [136, 60, 76, 28, 32, 36, 40],
   [36, 14, 22, 6, 8, 10, 12],
   [100, 46, 54, 22, 24, 26, 28],
   [10, 3, 7, 1, 2, 3, 4],
@@ -123,10 +141,18 @@ var eee = [
   [42, 19, 23, 9, 10, 11, 12],
   [58, 27, 31, 13, 14, 15, 16],
 ];
-var g = segmentTree(ar, sum, 0);
-g = g(0, 2)(0, 2);
-g = g(0, 2);
 
+
+var tree = recursiveSegmentTree(ar, sum, 0);
+var result = [];
+result.push(tree(0, 1)(0, 1)(0, 1));
+// result.push(tree(3, 4)(0, 1)(0, 1));
+// result.push(tree(1, 2)(3, 4)(1, 2));
+// result.push(tree(2, 3)(0, 4)(0, 2));
+// result.push(tree(2, 3)(1, 4)(0, 2));
+// result.push(tree(0, 4)(2, 3)(0, 2));
+// result.push(tree(0, 4)(0, 4)(0, 2));
+result = 0;
 function getElfTree(array) {
   return recursiveSegmentTree(array, sum, 0);
 }
